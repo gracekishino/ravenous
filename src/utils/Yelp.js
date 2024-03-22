3b3d97f6c9ff3579252fd0a1c2225c7e48018370
Grace Kishino <gracekishino@gmail.com>
2024-03-22 16:23:11 +0900

    Fix deploy

--- src/utils/Yelp.js
+++ src/utils/Yelp.js
@@ -1,14 +1,6 @@
-const apiKey = process.env.REACT_APP_YELP_API_KEY
 
-const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer ' + apiKey
-    }
-};
   
-const getBusinesses = async (location, term, sortBy) => {
+const getBusinesses = () => {
     return {"businesses": []};
 }
 
