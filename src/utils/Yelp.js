9ceb21011e835995b6a83238ca3b99889bd223cb
Grace Kishino <gracekishino@gmail.com>
2024-03-22 15:43:32 +0900

    FIx deploy

--- src/utils/Yelp.js
+++ src/utils/Yelp.js
@@ -9,12 +9,7 @@ const options = {
 };
   
 const getBusinesses = async (location, term, sortBy) => {
-// testing netlify function
-    await fetch('/.netlify/functions/fetch-businesses?location=London&term=cafe')
-    .then(response => console.log(response));
-
-
-    const endpoint = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${location}&term=${term}&sort_by=${sortBy}&limit=20`;
+    const endpoint = `https://api.yelp.com/v3/businesses/search?location=${location}&term=${term}&sort_by=${sortBy}&limit=20`;
 
     const response = await fetch(endpoint, options)
     .then(response => response.json())
