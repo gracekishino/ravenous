fae606d809083309f500ed3eca8fef1bf85908a3
Grace Kishino <gracekishino@gmail.com>
2024-03-22 16:49:01 +0900

    Fix deploy

--- functions/fetch-businesses/fetch-businesses.mjs
+++ functions/fetch-businesses/fetch-businesses.mjs
@@ -1,5 +1,6 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 
 export default async (req, context) => {
+  // const apiKey = Netlify.env.get("REACT_APP_YELP_API_KEY");
   return new Response("Hello, world!");
 };
