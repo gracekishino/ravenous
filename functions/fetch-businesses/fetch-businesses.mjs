1a15539666b4ebf33385b348f69878f5dec0d60c
Grace Kishino <gracekishino@gmail.com>
2024-03-29 21:55:12 +0900

    Tidy code

--- functions/fetch-businesses/fetch-businesses.mjs
+++ functions/fetch-businesses/fetch-businesses.mjs
@@ -1,9 +1,10 @@
-export default async (req) => {
-    const url = new URL(req.url);
+export default async (request) => {
+    const url = new URL(request.url);
     const location = url.searchParams.get("location") ?? "";
     const term = url.searchParams.get("term") ?? "";
     const sort_by = url.searchParams.get("sort_by") ?? "rating";
     const limit = url.searchParams.get("limit") ?? "20";
+    
     const apiKey = Netlify.env.get("REACT_APP_YELP_API_KEY");
     const options = {
         method: 'GET',
@@ -15,12 +16,12 @@ export default async (req) => {
   
     const endpoint = `https://api.yelp.com/v3/businesses/search?location=${location}&term=${term}&sort_by=${sort_by}&limit=${limit}`;
 
-    const response = await fetch(endpoint, options)
-    .then(response => response.json())
-    .catch(err => {
-        console.error(err);
-        return { statusCode: 500, body: error.toString() }
-    });
-
-    return new Response(JSON.stringify(response));
+    try {
+        const response = await fetch(endpoint, options);
+        const data = await response.json();
+        return Response.json(data);
+      } catch (error) {
+        console.log(error);
+        return Response.json({ error: 'Failed fetching data' }, { status: 500 });
+      }
 };
