45008106c295c82f3b0ed3c18563780e1c6d5ea2
Grace Kishino <gracekishino@gmail.com>
2024-03-29 22:28:58 +0900

    Add links to businesses

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -4,13 +4,17 @@ function Business({ business }) {
 
     return (
         <div className={styles.business} >
-            <img src={business.image_url} alt={business.name} />
+            <a href={business.url} target="_blank">
+                <img src={business.image_url} alt={business.name} />
+            </a>
             <div className={styles.name}>{business.name}</div>
-            <div className={styles.left}>
-                <div>{business.location["address1"]}</div>
-                <div>{business.location["city"]}</div>
-                <div>{business.state} {business.location["zip_code"]}</div>
-            </div>
+                <a href={`https://www.google.com/maps/search/?api=1&query=${business.name}+${business.location["city"]}+${business.state}`} target="_blank">
+                    <div className={styles.left}>
+                        <div>{business.location["address1"]}</div>
+                        <div>{business.location["city"]}</div>
+                        <div>{business.state} {business.location["zip_code"]}</div>
+                    </div>
+                </a>
             <div className={styles.right}>
                 <div className={styles.category}>{business.categories[0]["title"]}</div>
                 <div className={styles.rating}>{business.rating} stars</div>
